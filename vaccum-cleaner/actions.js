c729a42dbdaa66de09e04bb39203dca436473a82
silventino <silventinogato@gmail.com>
2018-09-09 00:49:36 -0300

    Robo manual funcionando. Vou tentar implementar a IA para que ele seja automatico.

--- vaccum-cleaner/actions.js
+++ vaccum-cleaner/actions.js
@@ -1,4 +1,11 @@
+var roomTheRobotIs = 0;
+var rooms = [true, true, true, true];
+
 $(document).ready(function() {
+
+
+	
+
 	$('#RIGHT').click(function() {
 		right();
 	});
@@ -16,21 +23,70 @@ $(document).ready(function() {
 	});
 
 	$('.room').click(function() {
-	})
+		// alert(this.id);
+		rooms[this.id] = false;
+		$("#"+this.id).css("background-color", "red");
+		cleanRoom(roomTheRobotIs);
+	});
+
+
 });
 
+async function cleanRoom(roomId){
+	if(rooms[roomId] == false){
+		$('#action').text("Clening...")
+	}
+	setTimeout(
+		() => {
+			$('#action').text("")
+			$("#"+roomId).css("background-color", "white");
+			rooms[roomId] = true;
+
+		}, 
+		1000);
+}
+
 function right() {
+	if(roomTheRobotIs == 0){
+		roomTheRobotIs = 1;
+	}
+	if(roomTheRobotIs == 2){
+		roomTheRobotIs = 3;
+	}
+	cleanRoom(roomTheRobotIs);
 	$('#r1').animate({left: '250px'});
 }
 
+
 function left() {
+	if(roomTheRobotIs == 1){
+		roomTheRobotIs = 0;
+	}
+	if(roomTheRobotIs == 3){
+		roomTheRobotIs = 2;
+	}
+	cleanRoom(roomTheRobotIs);
 	$('#r1').animate({left: '0px'});
 }
 
 function down() {
+	if(roomTheRobotIs == 0){
+		roomTheRobotIs = 2;
+	}
+	if(roomTheRobotIs == 1){
+		roomTheRobotIs = 3;
+	}
+	cleanRoom(roomTheRobotIs);
 	$('#r1').animate({top: '250px'});
 }
 
 function up() {
+	if(roomTheRobotIs == 2){
+		roomTheRobotIs = 0;
+	}
+	if(roomTheRobotIs == 3){
+		roomTheRobotIs = 1;
+	}
+	cleanRoom(roomTheRobotIs);
 	$('#r1').animate({top: '0px'});
 }
