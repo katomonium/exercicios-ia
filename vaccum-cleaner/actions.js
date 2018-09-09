c512928e171ffd7e4ad79237655420cf3cd4460d
silventino <silventinogato@gmail.com>
2018-09-09 01:26:50 -0300

    Modo automatico funcionando, quero fazer uma IA mais legal agora que consiga decidir entre 2 quartos qual é o mais sujo.

--- vaccum-cleaner/actions.js
+++ vaccum-cleaner/actions.js
@@ -1,10 +1,7 @@
 var roomTheRobotIs = 0;
 var rooms = [true, true, true, true];
 
-$(document).ready(function() {
-
-
-	
+$(document).ready(function() {	
 
 	$('#RIGHT').click(function() {
 		right();
@@ -28,22 +25,23 @@ $(document).ready(function() {
 		$("#"+this.id).css("background-color", "red");
 		cleanRoom(roomTheRobotIs);
 	});
-
+	automaticMode();
 
 });
 
-async function cleanRoom(roomId){
+function cleanRoom(roomId){
 	if(rooms[roomId] == false){
-		$('#action').text("Clening...")
-	}
-	setTimeout(
-		() => {
-			$('#action').text("")
-			$("#"+roomId).css("background-color", "white");
-			rooms[roomId] = true;
+		$('#action').text("Cleaning...")
+		setTimeout(
+			() => {
+				$('#action').text("")
+				$("#"+roomId).css("background-color", "white");
+				rooms[roomId] = true;
 
-		}, 
-		1000);
+			}, 
+			1000
+		);
+	}
 }
 
 function right() {
@@ -89,4 +87,45 @@ function up() {
 	}
 	cleanRoom(roomTheRobotIs);
 	$('#r1').animate({top: '0px'});
+}
+
+function automaticMode(){
+	if(roomTheRobotIs == 0){
+		
+	}
+	switch(roomTheRobotIs){
+		case 0:
+			cleanRoom(0);
+			right();
+			setTimeout(
+				() => {automaticMode()},
+				2000
+			);
+			break;
+		case 1:
+			cleanRoom(1);
+			down();
+			setTimeout(
+				() => {automaticMode()},
+				2000
+			);
+			break;
+		case 2:
+			cleanRoom(2);
+			up();
+			setTimeout(
+				() => {automaticMode()},
+				2000
+			);
+			break;
+		case 3:
+			cleanRoom(3);
+			left();
+			setTimeout(
+				() => {automaticMode()},
+				2000
+			);
+			break;
+
+	}
 }
