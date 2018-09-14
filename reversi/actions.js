515a0917d9d0271a0bb051587f5b8caf4ee61015
KatoMono Enkeli <k4t0mono@gmail.com>
2018-09-14 13:24:54 -0300

    inspect_table consertado

--- reversi/actions.js
+++ reversi/actions.js
@@ -7,20 +7,20 @@ for(var i = 0; i < 8; i++) {
 }
 
 const inspect_table = () => {
-	let s = '    0 1 2 3 4 5 6 7';
-	console.log(s);
+	let s = '    0 1 2 3 4 5 6 7\n';
 
 	for(var i = 0; i < 8; i++) {
-		let s = '[ ';
+		s += `${i} [ `;
 		for(var j = 0; j < 8; j++) {
 			if(TABLE[i][j] === '')
 				s += '· '
 			else
 				s += `${TABLE[i][j]} `;
 		}
-		s += ']'
-		console.log(`${i} ${s}`)
+		s += ']\n'
 	}
+
+	console.log(s);
 }
 
 let PLAYER = true;
