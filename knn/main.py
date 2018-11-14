426960939fc9d5884063a81690c00df4555212bd
KatoMono Enkeli <k4t0mono@gmail.com>
2018-11-13 23:58:59 -0200

    Fazendo cross validation

--- knn/main.py
+++ knn/main.py
@@ -1,11 +1,29 @@
 from pprint import pprint
 from knn import KNN
+from validator import Validator
 import csv
+import json
+
+
+def load_dataset(path):
+    dataset = []
+    with open(path) as fl:
+        reader = csv.reader(fl, delimiter=',')
+
+        for row in reader:
+            instance = []
+            for i in range(len(row) - 1):
+                instance.append(float(row[i]))
+
+            instance.append(int(row[57]))
+            dataset.append(instance)
+    
+    return dataset
 
 
 if __name__ == '__main__':
-    dt = open('dataset/spambase.data').readlines()
-    ln = dt[0].strip()
-
-    for r in csv.reader(ln):
-        print(r)
+    dataset = load_dataset('dataset/spambase.random.data')
+    v = Validator(dataset)
+    v.cross_validation(k=3)
+    print(v.results)
+    #v.calc_statis(json.loads(open('r.json').read()))
---
+++ knn/validator.py
@@ -0,0 +1,70 @@
+from knn import KNN
+import sys
+import json
+
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+
+
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
+
+class Validator:
+
+    def __init__(self, dataset, n_chunks=5):
+        self.dataset = dataset
+        self.n_chunks = n_chunks
+        self.chunks = list(split(dataset, n_chunks))
+        self.results = []
+    
+    def validate_test(self, knn, test, k, i):
+        count = 0
+        results = []
+        for d in test:
+            r = knn.classify(d, k)
+            results.append((d[-1], r))
+            count += 1
+            progress(count, len(test), 'validating test_set {}'.format(i))
+
+        return results
+
+    def cross_validation(self, k):
+        for i in range(self.n_chunks):
+            l = self.chunks[:i] + self.chunks[i+1:]
+            training = [item for sublist in l for item in sublist] 
+            knn = KNN(training)
+
+            test = self.chunks[i]
+            results = self.validate_test(knn, test, k, i)
+            s = self.calc_statis(results)
+            self.results.append(s)
+
+    def calc_statis(self, results):
+        s = { 'tp': 0, 'fp': 0, 'tn': 0, 'fn': 0 }
+
+        for r in results:
+            if r[0]:
+                if r[1]:
+                    s['tp'] += 1
+                else:
+                    s['fn'] += 1
+            else:
+                if r[1]:
+                    s['fp'] += 1
+                else:
+                    s['tn'] += 1
+        p = s['tp'] / (s['tp'] + s['fp'])
+        r = s['tp'] / (s['fn'] + s['tp'])
+        f1 = 2 * (p * r) / (p + r)
+
+        return (p, r, f1)
